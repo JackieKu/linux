47c38829bebd2c7adb9cbaae08219b19c28f3b31
Kai-Chieh Ku <kjackie@gmail.com>
2015-11-10 22:56:50 +0800

    pcm: #include <linux/uio.h>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -26,6 +26,7 @@
 #include <linux/time.h>
 #include <linux/pm_qos.h>
 #include <linux/aio.h>
+#include <linux/uio.h>
 #include <linux/dma-mapping.h>
 #include <sound/core.h>
 #include <sound/control.h>
