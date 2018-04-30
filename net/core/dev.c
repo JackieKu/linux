655c4627e9b9dd753856a676975d9c0fbe36069c
Kai-Chieh Ku <kjackie@gmail.com>
2018-04-30 13:19:39 +0800

    linux-4.4.32-imq.diff
    Minor patch in nf_queue() in nf_queue.c:
    rcu_dereference(queue_handler) -> rcu_dereference(net->nf.queue_handler)

--- net/core/dev.c
+++ net/core/dev.c
@@ -137,6 +137,9 @@
 #include <linux/errqueue.h>
 #include <linux/hrtimer.h>
 #include <linux/netfilter_ingress.h>
+#if defined(CONFIG_IMQ) || defined(CONFIG_IMQ_MODULE)
+#include <linux/imq.h>
+#endif
 
 #include "net-sysfs.h"
 
@@ -2751,7 +2754,12 @@ static int xmit_one(struct sk_buff *skb, struct net_device *dev,
 	unsigned int len;
 	int rc;
 
+#if defined(CONFIG_IMQ) || defined(CONFIG_IMQ_MODULE)
+	if ((!list_empty(&ptype_all) || !list_empty(&dev->ptype_all)) &&
+		!(skb->imq_flags & IMQ_F_ENQUEUE))
+#else
 	if (!list_empty(&ptype_all) || !list_empty(&dev->ptype_all))
+#endif
 		dev_queue_xmit_nit(skb, dev);
 
 	len = skb->len;
@@ -2789,6 +2797,7 @@ out:
 	*ret = rc;
 	return skb;
 }
+EXPORT_SYMBOL_GPL(dev_hard_start_xmit);
 
 static struct sk_buff *validate_xmit_vlan(struct sk_buff *skb,
 					  netdev_features_t features)
